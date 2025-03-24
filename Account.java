1169dff24527b7e186187eb655c117867492e179
Kanishka Malik <malikkanishka165@gmail.com>
2025-03-24 19:37:49 +0530

    Version 1.1

--- Account.java
+++ Account.java
@@ -1,51 +1,51 @@
 package CaseStudy2;
 
 public class Account {
-	private long accNumber = 12345678l;
-	private double balance = setAccBalance(getBalancer());
-	private String name = "Kanishka Malik";
-
+		private long accNumber;
+	private double balance;
+	private String name;
 	// getter
 	public long getAccNumber() {
 		return this.accNumber;
-
 	}
-
 	public double getBalancer() {
 		return this.balance;
-
 	}
-
 	public String getAccName() {
 		return this.name;
-
 	}
-
 	// setter
 	public void setAccName(String name) {
 		this.name = name;
-
 	}
-
 	public double setAccBalance(double balance) {
-
 		return this.balance = balance;
-
 	}
-
 	// depositeMethod
-	public double depositeAcc(double amount) {
+	public Account depositeAcc(double amount) {
+		if (amount>0) {
+			setAccBalance(getBalancer() + amount);
+			
+		}
+		else {
+			System.out.println("Invalid amount!");
+		}
+		return this;
 		
-		double newBlaance=getBalancer() + amount;
-		return setAccBalance(newBlaance);
-
+		
+		
 	}
-
 	// withdrawMethod
-	public double withdrawAcc(double amount) {
+	public Account withdrawAcc(double amount) {
 		
-		double newBlaance=getBalancer() - amount;
-		return setAccBalance(newBlaance);
+		if (amount>0 && amount<getBalancer()) {
+			setAccBalance(getBalancer() - amount);
+			
+		}
+		else {
+			System.out.println("Invalid amount!");
+		}
+		return this;
 	}
 
 }
